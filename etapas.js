3c81062cf204a6f3e3a80ee03802b6454b829810
Luan <165568493+LuanSalesx@users.noreply.github.com>
2024-06-26 07:32:08 -0300

    added steps with script.js properties

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '13222',
+                nome: 'Cachorrão',
+                partido: 'PT',
+                fotos: [
+                    {url:'vereador_2.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77213',
+                nome: 'Mano Esquilo',
+                partido: 'PT',
+                fotos: [
+                    {url:'vereador_1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '12113',
+                nome: 'Eva Sales',
+                partido: 'PT',
+                fotos: [
+                    {url: 'vereador_3.jpg', legenda: 'Vereadora'}
+                ]
+            },
+            {
+                numero: '13131',
+                nome: 'Leco Teco',
+                partido: 'PT',
+                fotos: [
+                    {url: 'vreador_4.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Gorducho',
+                partido: 'PT',
+                vice: 'Monarka',
+                fotos: [
+                    {url: 'prefeito.jpg', legenda: 'Prefeito'},
+                    {url: 'vice_prefeito.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Luan Boladão',
+                partido: 'PKT',
+                vice: 'Aldair da Senha',
+                fotos: [
+                    {url: 'prefeito_2.jpg', legenda: 'Prefeito'},
+                    {url: 'vice_prefeito_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
